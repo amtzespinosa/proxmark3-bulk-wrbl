a99c4c4828369579cf730d08bfa6fb5c80506988
Alex Martínez <76045216+amtzespinosa@users.noreply.github.com>
2023-05-13 17:28:47 +0200

    Update bulk_wrbl.py

--- bulk_wrbl.py
+++ bulk_wrbl.py
@@ -2,19 +2,19 @@ import subprocess
 
 # Command to write a specific block with a specific key
 def write_block(block_data, key, block_number):
-    command = f'hf mf wrbl {block_number} A {key} {block_data}'
-    subprocess.run(['proxmark3', '-c', command], check=True, capture_output=True, text=True)
+	init = "./proxmark3 /dev/ttyACM0 -c "
+	write = "'hf mf wrbl '" + f"'--blk {block_number} -k {key} -d {block_data}'"
+	command = init + write
+	
+	result = subprocess.run(command, shell=True, capture_output=True, text=True)
+	print(result.stdout) # Just for checking
 
 # Sample data
 blocks_to_write = [
-    {'block_data': '1122334455667788', 'key': 'FFFFFFFFFFFF', 'block_number': 16},
-    {'block_data': 'AABBCCDDEEFF0011', 'key': '000000000000', 'block_number': 18},
-    # Add blocks as needed
+    {'block_data': '11223344556677881122334455667788', 'key': 'FFFFFFFFFFFF', 'block_number': 16},
+    {'block_data': 'AABBCCDDEEFF0011AABBCCDDEEFF0011', 'key': '000000000000', 'block_number': 18},
+	# Add blocks as needed
 ]
 
-# Write blocks with the possibility of excluding some
-excluded_blocks = [17, 20]  # Add here the blocks you want to excluse 
-
 for block in blocks_to_write:
-    if block['block_number'] not in excluded_blocks:
-        write_block(block['block_data'], block['key'], block['block_number'])
+	write_block(block['block_data'], block['key'], block['block_number'])
